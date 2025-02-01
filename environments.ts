1a52240c8fa5decc418cb5d83ad8a4bdc450442e
Abdulrahman Khaled Adam <abdulrahmankhadamm@gmail.com>
2025-02-01 17:31:16 +0200

    Updated the api url

--- environments.ts
+++ environments.ts
@@ -1,5 +1,4 @@
 export const env = {
-  API_URL:
-    'https://serverabdulrahmanadam-abdulrahmanadams-projects.vercel.app/api/v1',
+  API_URL: 'http://16.171.136.91:80/api/v1',
   SERVER_URL: '',
 };
